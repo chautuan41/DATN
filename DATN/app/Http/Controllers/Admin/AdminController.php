427f5dcff6ccf7478436a618f718751ed18eb416
quangthien167 <quangthien167@gmail.com>
2022-06-17 23:41:27 +0700

    Create Login Admin

---
+++ DATN/app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Account;
+
+class AdminController extends Controller
+{
+    public function indexAdmin(){
+        return view('dashboard.layout.dashboard-admin');
+    }
+    // public function indexAdminDH(){
+    //     return view('layouts.adminwatches');
+    // }
+    // public function indexAdminCL(){
+    //     return view('layouts.adminclothing');
+    // }
+}
---
+++ DATN/app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+ /**
+ * Where to redirect admins after login.
+ *
+ * @var string
+ */
+ protected $redirectTo = '/admin';
+ /**
+ * Create a new controller instance.
+ *
+ * @return void
+ */
+ public function __construct()
+ {
+ $this->middleware('guest:admin')->except('logout');
+ }
+ /**
+ * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+ */
+public function showLoginForm()
+{
+    return view('dashboard.auth.page-login');
+}
+
+public function index()
+{
+    return view('dashboard.index');
+}
+
+public function login(Request $request)
+    {
+        if (Auth::guard('admin')->attempt([
+        'email' => $request->email,
+        'password' => $request->password,
+        'role' => 2,
+        'status' => 1,
+        ], $request->get('remember'))) {
+            return redirect()->intended(route('admin.index'));
+        }
+        // elseif (Auth::guard('admin')->attempt([
+        //     'email' => $request->email,
+        //     'password' => $request->password,
+        //     'role' => 3,
+        //     'status' => 1,
+        //     ], $request->get('remember'))) {
+        //         return redirect()->intended(route('admin.indexDH'));
+        //     }
+        // elseif (Auth::guard('admin')->attempt([
+        //         'email' => $request->email,
+        //         'password' => $request->password,
+        //         'role' => 4,
+        //         'status' => 1,
+        //         ], $request->get('remember'))) {
+        //             return redirect()->intended(route('admin.indexCL'));
+        //         }
+        else{
+            return redirect()->back()->with("error","Đăng nhập không thành công");
+        }
+            
+        return back()->withInput($request->only('email', 'remember'));
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+        $request->session()->invalidate();
+        return redirect()->route('admin.login');
+    }
+}
